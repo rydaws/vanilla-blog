56300030b4e87e7b2022d3049fc247b2a50965d5
rdaws <dev.rdawson@gmail.com>
2025-08-26 20:08:13 -0400

    fix dark mode, update logo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-const toggleButton = document.getElementById('theme-toggle');
+const toggleButton = document.getElementById("theme-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-toggleButton.addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
+toggleButton.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+  } else {
+    document.body.classList.toggle("dark-theme");
+  }
 });
